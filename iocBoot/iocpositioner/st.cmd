bdc9931f1f9a44f9fdedc224967cbce94a431a77
Pete Leicester <Pete.Leicester@diamond.ac.uk>
2005-09-08 15:33:41 +0000

    Initial version.

--- iocBoot/iocpositioner/st.cmd
+++ iocBoot/iocpositioner/st.cmd
@@ -3,6 +3,9 @@
 ## You may have to change positioner to something else
 ## everywhere it appears in this file
 
+epicsEnvSet(EPICS_CA_REPEATER_PORT,"6065")
+epicsEnvSet(EPICS_CA_SERVER_PORT,"6064")
+
 < envPaths
 
 cd ${TOP}
@@ -12,7 +15,14 @@ dbLoadDatabase("dbd/positioner.dbd",0,0)
 positioner_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=pjl45Host")
+#dbLoadRecords("db/example.db","user=pjl45")
+cd db
+dbLoadTemplate("$(TOP)/example/example.substitutions","user=pjl45")
+#dbLoadRecords("$(TOP)/example/example_support.db","user=pjl45")
+
+# Configure simulation PMAC
+var simulation_mode 1
+PmacSetup(0,0,0,0,0,0,0)
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit()
